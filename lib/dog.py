59d583675ec78a9d3ccc839878dc0f5ecd1b9ce3
ian <ianmwendamicheni@gmail.com>
2023-12-05 17:57:04 +0300

    translating

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,37 @@
 from models import Dog
 
-def create_table(base):
+def create_table(base ,engine):
+    base.metadata.create_all(engine)
     pass
 
 def save(session, dog):
+    session.add(dog)
+    session.commit()
     pass
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    try:
+        return session.query(Dog).filter_by(name=name).one()
+    except NoResultFound:
+        return None
 
-def find_by_id(session, id):
-    pass
+def find_by_id(session, dog_id):
+    try:
+        return session.query(Dog).filter_by(id=dog_id).one()
+    except NoResultFound:
+        return None
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    try:
+        return session.query(Dog).filter_by(name=name, breed=breed).one()
+    except NoResultFound:
+        return None
 
-def update_breed(session, dog, breed):
-    pass
+def update_breed(session, dog, new_breed):
+    dog.breed = new_breed
+    session.commit()
+
+# ... (remaining code)
